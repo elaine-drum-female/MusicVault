29cd250955c1f7c0990b7945662db1bc999362d6
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 20:18:27 -0500

    Added bycrypt and salt code

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
     email: {
@@ -39,6 +41,23 @@ const userSchema = mongoose.Schema({
         type:String
     }
 
+});
+
+//Saving this before going through server
+userSchema.pre('save', function(next) {
+    // Referencing this user and not the function
+    var user = this;
+    bcrypt.genSalt(SALT_I, function(err, salt) {
+        // kills process of whatever is being run (i.e. posting req) before moving forward
+        if(err) return next (err);
+
+        // Use password of user to hash out password
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const User = mongoose.model('User', userSchema);
