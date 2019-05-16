86a85c01d4ee3ced933de0324bd1b2abe4ef4a2a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 00:25:44 -0500

    Updated the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import axios from 'axios';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-     MY APP
-    </div>
-  );
+// componentDidMount() {
+//   axios.get('/api/product/brands').then(response=> {
+//     console.log(response);
+//   });
+// }
+
+  render() {
+    return (
+      <div className="App">
+        MY APP
+      </div>
+    );
+  }
 }
 
 export default App;
