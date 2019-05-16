135a32f7eeeea884159332d80020dc6cc65cefd6
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 15:13:44 -0500

    Added the rest of the state

--- client/src/components/Register_login/register.js
+++ client/src/components/Register_login/register.js
@@ -10,7 +10,70 @@ class Register extends Component {
         formError: false,
         formSuccess:'',
         formdata:{
+            name: {
+                element: 'input',
+                value: '',
+                config:{
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Please enter your name'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
+            },
 
+            lastname: {
+                element: 'input',
+                value: '',
+                config:{
+                    name: 'lastname_input',
+                    type: 'text',
+                    placeholder: 'Please enter your lastname'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
+            },
+
+            password: {
+                element: 'input',
+                value: '',
+                config:{
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
+            },
+
+            confirmPassword: {
+                element: 'input',
+                value: '',
+                config:{
+                    name: 'confirm_password_input',
+                    type: 'password',
+                    placeholder: 'Confirm password'
+                },
+                validation:{
+                    required: true,
+                    confirm: 'password'
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
+            }
         }
     }
 
