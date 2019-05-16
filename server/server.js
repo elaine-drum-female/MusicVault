5e225fd04b96e510ea53beb9560491bd297f952f
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 19:37:23 -0500

    Setup the bodyParser,cookieParser, Mongoose

--- server/server.js
+++ server/server.js
@@ -2,6 +2,22 @@
 const express = require('express');
 const app = express();
 
+// Added Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Added Cookie Parser
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// Added Mongoose
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
 const port = process.env.PORT || 3005;
 
 app.listen(port , () => {
