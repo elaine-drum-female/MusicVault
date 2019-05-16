d23b2fe1d0ebcb74a1a51ec10715cc4b7b956a16
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 15:09:06 -0500

    Created the Register component started with state

---
+++ client/src/components/Register_login/register.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+// import FormField from '../utils/Form/formfield';
+// import { update, generateData, isFormValid } from '../utils/Form/formActions';
+import { connect } from 'react-redux';
+// import { loginUser } from '../../actions/user_actions';
+
+class Register extends Component {
+    state = {
+        formError: false,
+        formSuccess:'',
+        formdata:{
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Register
+            </div>
+        );
+    }
+}
+
+export default connect()(Register);
