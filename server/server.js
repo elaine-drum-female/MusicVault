d817991800388c1ec7cb65b416338c8b0bad7e0a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 19:29:57 -0500

    Added the PORT

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 // Added Express Server
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3005;
+
+app.listen(port , () => {
+    console.log(`Server running at ${port}`);
+});
