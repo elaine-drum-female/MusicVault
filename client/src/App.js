b49938f473fb143eb3e9e09e628c4f22036f8185
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 12:51:46 -0500

    Imported register route

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Wrapper from './hoc/wrapper';
 import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/register';
 
 const App = () => {
   return (
     <Wrapper>
       <Switch>
+        <Route exact path="/register" component={Register} />
         <Route exact path="/register_login" component={RegisterLogin} />
         <Route exact path="/" component={Home} />
       </Switch>
