c4b76b603ebc48c58ddd4c322fa271eb0b2a8751
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 13:38:05 -0500

    Changed name to Music Vault

--- client/src/components/Header_Footer/Header/index.js
+++ client/src/components/Header_Footer/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
                 <div className="container">
                     <div className="left">
                         <div className="logo">
-                            WAVES
+                            MUSICVAULT
                         </div>
                     </div>
                     <div className="right">
