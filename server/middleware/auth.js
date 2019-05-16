e405bb39a9abb13492853ce010ae61879da7c3fa
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 21:29:09 -0500

    Exported module auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const { User } = require('./../models/user');
+let auth = (req, res, next) => {
+    let token = req.cookies.x_auth;
+    User.findByToken(token, (err, user) => {
+        if(err) throw err;
+        if(!user) return res.json({
+            isAuth: false,
+            error: true
+        });
+
+        // Pushes value of token
+        req.token = token;
+        // Attach data to the request
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { auth }
