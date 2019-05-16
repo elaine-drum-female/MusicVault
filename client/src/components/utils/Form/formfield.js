9979de1a0bfb5d9f2f06849be7c707d988c66822
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 10:49:01 -0500

    Setting up errors to work with input fields

---
+++ client/src/components/utils/Form/formfield.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Formfield = ({id, formdata, change}) => {
+
+
+    const showError = () => {
+        let errorMessage = null;
+
+        if(formdata.validation && !formdata.valid){
+            errorMessage = (
+                <div className="error_label">
+                    {formdata.validationMessage}
+                </div>
+            )
+        }
+
+        return errorMessage;
+    }
+
+
+    const renderTemplate = () => {
+        let formTemplate = null;
+
+        switch(formdata.element){
+            case('input'):
+                formTemplate = (
+                    <div className="formBlock">
+                        <input
+                            {...formdata.config}
+                            value={formdata.value}
+                            onChange={(event)=> change({event,id}) }
+                        />
+                        {showError()}
+                    </div>
+                )
+            break;
+            default:
+                formTemplate = null;
+        }
+
+        return formTemplate;
+    }
+
+
+    return (
+        <div>
+            {renderTemplate()}
+        </div>
+    );
+};
+
+export default Formfield;
