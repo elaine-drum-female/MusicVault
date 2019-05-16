5ec888795a8ff2dbdadb8673e55e6021e1b8e60a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 20:07:51 -0500

    Added new User , updated Mongoose with MONGOD_URI

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/waves");
 
 // Models
 const { User } = require('./models/user');
@@ -27,7 +27,14 @@ const { User } = require('./models/user');
 //==================================
 
 app.post('/api/users/register', (req, res) => {
-    res.send(200);
+   const user = new User(req.body);
+   user.save((err, doc) => {
+    if(err) return res.json({success: false, err});
+    res.status(200).json({
+        success:true,
+        userData: doc
+    });
+   });
 });
 
 
