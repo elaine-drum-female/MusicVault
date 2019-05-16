628225f5888ef4b833afda9d79d8f39f3d65e9b2
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 00:52:13 -0500

    Wrapped Wrapper comp around Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Wrapper from './hoc/wrapper';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <Wrapper>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Wrapper>
   );
 };
 
