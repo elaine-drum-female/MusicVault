46fab1db39b91a8a525f24c04bbb7e83055acc33
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 10:09:55 -0500

    Added properties inside LinkButton

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinkButton from "../utils/button";
 
 const RegisterLogin = () => {
     return (
@@ -8,6 +9,14 @@ const RegisterLogin = () => {
                    <div className="left">
                        <h1>New Customers</h1>
                        <p>Lorem ipsum dolor amet iPhone lyft ethical, umami coloring book fashion axe dreamcatcher gluten-free swag hoodie green juice yuccie glossier vaporware. XOXO pok pok shaman, shabby chic chicharrones drinking vinegar kale chips. Af PBR&B aesthetic squid unicorn mlkshk roof party keffiyeh actually humblebrag single-origin coffee cray waistcoat. Lumbersexual leggings put a bird on it poke cold-pressed next level authentic cronut. Jean shorts roof party bespoke, fam live-edge vape twee. </p>
+                       <LinkButton 
+                            type="default"
+                            title="Make an account"
+                            linkTo="/register"
+                            addStyles={{
+                                margin:'10px 0 0 0'
+                            }}
+                       />
                    </div>
                    <div className="right">
 
