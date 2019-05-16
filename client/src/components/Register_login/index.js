22e605d58a5cc64be897ac94ad9bf4ef31846a2c
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 10:01:40 -0500

    Added code to RegisterLogin starting with left

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const RegisterLogin = () => {
     return (
-        <div>
-            register
+        <div className="page_wrapper">
+            <div className="container">
+               <div className="register_login_container">
+                   <div className="left">
+                       <h1>New Customers</h1>
+                       <p>Lorem ipsum dolor amet iPhone lyft ethical, umami coloring book fashion axe dreamcatcher gluten-free swag hoodie green juice yuccie glossier vaporware. XOXO pok pok shaman, shabby chic chicharrones drinking vinegar kale chips. Af PBR&B aesthetic squid unicorn mlkshk roof party keffiyeh actually humblebrag single-origin coffee cray waistcoat. Lumbersexual leggings put a bird on it poke cold-pressed next level authentic cronut. Jean shorts roof party bespoke, fam live-edge vape twee. </p>
+                   </div>
+                   <div className="right">
+
+                   </div>
+               </div> 
+            </div>
         </div>
     );
 };
