e77240f23e0fe74b288d6ba8b31bbcd714b29563
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 01:00:52 -0500

    Imported Header

--- client/src/hoc/wrapper.js
+++ client/src/hoc/wrapper.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import Header from '../components/Header_Footer/Header';
 
 class Wrapper extends Component {
     render() {
         return (
             <div>
-                 HEADER
+                 <Header/>
                 <div className="page_container">
                     {this.props.children}
                 </div>
-                FOOTER
+                Footer
             </div>
         );
     }
