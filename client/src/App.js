346cec761eb00c2f3a9d023ce80547533c5651dd
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 00:46:46 -0500

    Added Switch, Route and path to Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 
-class App extends Component {
-
-componentDidMount() {
-  axios.get('/api/product/getbrands').then(response=> {
-    console.log(response);
-  });
-}
-
-  render() {
-    return (
-      <div className="App">
-        MY APP
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
+};
 
 export default App;
