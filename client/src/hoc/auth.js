427d09bc56a9fc7173613efb6f9735185bc1cb26
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-17 14:17:35 -0500

    Added the CircularProcess loader

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { Authorize } from '../actions/user_actions';
-// import CircularProcess from '@material-ui/core/CircularProgress';
+//import { Authorize } from '../actions/user_actions';
+import CircularProcess from '@material-ui/core/CircularProgress';
 
 export default function(ComposedClass, reload, adminRoute = null) {
     class AuthCheck extends Component {
+
+        state = {
+            loading: true
+        }
+
         render() {
+            if(this.state.loading) {
+                return (
+                    <div className="main_loader">
+                        <CircularProcess style={{
+                            color:'#ccc'
+                        }} thickness={7}/>
+                    </div>
+                )
+            }
             return (
                 <div>
-                    
+                    component
                 </div>
             );
         }
     }
 
-    
+    function mapStateToProps(state) {
+        return {
+            user : state.user
+        }
+    }
 
-    return connect()(AuthCheck)
+    return connect(mapStateToProps)(AuthCheck)
     
 }
