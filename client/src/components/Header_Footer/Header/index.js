ca7616d8edca2eebca613132806b36ac8058a9d1
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-17 16:07:18 -0500

    Added state to go with the links for publiclinks and userlinks

--- client/src/components/Header_Footer/Header/index.js
+++ client/src/components/Header_Footer/Header/index.js
@@ -1,6 +1,49 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    state={
+        publiclinks: [
+            {
+                name:"Home",
+                linkTo:'/',
+                public:true
+            },
+
+            {
+                name:"Categories",
+                linkTo:'/shop',
+                public:true
+            }
+        ],
+
+        userlinks: [
+            {
+                name:"My Cart",
+                linkTo:'/user/shopping-cart',
+                public:false
+            },
+            {
+                name:"My Account",
+                linkTo:'/user/dashboard',
+                public:false
+            },
+           {
+                name:"Log In",
+                linkTo:'/register_login',
+                public:true
+            }, 
+           {
+                name:"Log Out",
+                linkTo:'/user/logout',
+                public:false
+            } 
+            
+        ]
+    }
+
+
+
     render() {
         return (
             <header className="bck_b_light">
