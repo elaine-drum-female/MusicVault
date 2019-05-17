73d89385d8e2ab255476c5dfd1db46d7c597bc96
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-17 12:08:21 -0500

    Added the User Information section on right

--- client/src/components/PrivateDashboard/index.js
+++ client/src/components/PrivateDashboard/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import UserLayout from '../../hoc/userlayout';
+// import LinkButton from '../utils/button';
 
 const UserDashboard = () => {
     return (
         <UserLayout>
             <div>
-                dashboard
+                <div className="user_nfo_panel">
+                    <h1>User Information</h1>
+                    <div>
+                        <span>name</span>
+                        <span>lastname</span>
+                        <span>email</span>
+                    </div>
+                    
+                </div>
             </div>
         </UserLayout>
     );
