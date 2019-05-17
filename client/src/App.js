6901a7ced19a0fff98f7bbf5979dabd9616df6bc
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 20:44:09 -0500

    Imported new route for user/dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Home from './components/Home';
 import Wrapper from './hoc/wrapper';
 import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/register';
+import UserDashboard from './components/PrivateUser';
 
 const App = () => {
   return (
     <Wrapper>
       <Switch>
+        <Route exact path="/user/dashboard" component={UserDashboard} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/register_login" component={RegisterLogin} />
         <Route exact path="/" component={Home} />
