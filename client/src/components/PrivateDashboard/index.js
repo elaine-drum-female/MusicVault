4eda3f6b6dbc8f0b2f90c215edc606daea39b817
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-17 12:04:46 -0500

    Renamed the file to PrivateDashboard

---
+++ client/src/components/PrivateDashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserLayout from '../../hoc/userlayout';
+
+const UserDashboard = () => {
+    return (
+        <UserLayout>
+            <div>
+                dashboard
+            </div>
+        </UserLayout>
+    );
+};
+
+export default UserDashboard;
