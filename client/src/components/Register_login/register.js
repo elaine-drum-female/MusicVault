084445b82e355f4de121376a1a4542c2e4118f62
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-16 20:32:36 -0500

    Dialog, formSuccess value; created setTimeout

--- client/src/components/Register_login/register.js
+++ client/src/components/Register_login/register.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import FormField from '../utils/Form/formfield';
 import { update, generateData, isFormValid } from '../utils/Form/formActions';
 import { connect } from 'react-redux';
-//import { loginUser } from '../../actions/user_actions';
+import { registerUser } from '../../actions/user_actions';
+import Dialog from '@material-ui/core/Dialog';
 
 class Register extends Component {
     state = {
         formError: false,
-        formSuccess:'',
+        formSuccess:false,
         formdata:{
             name: {
                 element: 'input',
@@ -108,14 +109,27 @@ class Register extends Component {
         let formIsValid = isFormValid(this.state.formdata,'register')
 
         if(formIsValid) {
-                console.log(dataToSubmit);
-               } else {
-                    this.setState({
-                        formError :true
-                    })
-               }
-           }
+                this.props.dispatch(registerUser(dataToSubmit))
+                .then(response => {
+                    if(response.payload.success) {
+                        this.setState({
+                            formError:false,
+                            formSuccess : true
+                        });
+                        setTimeout(() => {
+                            this.props.history.push('/register_login');
+                        }, 3000)
+                    } else {
+                        this.setState({formError: true})
+                    }
+                }).catch(e => {
+                    this.setState({formError: true})
+                })
+        } else {
+            this.setState({formError:true})
+        }
     
+    }
     
     render() {
         return (
@@ -181,6 +195,16 @@ class Register extends Component {
                         </div>
                     </div>
                 </div>
+
+                <Dialog open={this.state.formSuccess}>
+                    <div className="dialog_alert">
+                        <div>Thank you for creating an account.</div>
+                        <div>Please be aware you will be redirected to the LOGIN page in a couple of seconds</div>
+                    </div>
+                </Dialog>
+
+
+
             </div>
         );
     }
