7be473cad0a97751476ff80edbc0bfbc14a4e8e6
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 01:26:02 -0500

    Added the slide array with the featured images to use on homepage

--- client/src/components/Home/hero_slider.js
+++ client/src/components/Home/hero_slider.js
@@ -4,20 +4,40 @@ const HeroSlider = (props) => {
 
     const slides = [
         {
-            img:'',
-            lineOne:'',
-            lineTwo:'',
-            linkTitle:'',
-            linkTo:''
+            img:'/images/featured/featured_home.jpg',
+            lineOne:'Fender',
+            lineTwo:'Custom shop',
+            linkTitle:'Shop now',
+            linkTo:'/shop'
         },
-        
+
         {
-            img:'',
-            lineOne:'',
-            lineTwo:'',
-            linkTitle:'',
-            linkTo:''
-        }
+            img:'/images/featured/featured_home_2.jpg',
+            lineOne:'B-Stock',
+            lineTwo:'Awesome discounts',
+            linkTitle:'View offers',
+            linkTo:'/shop'
+        },
+
+        {
+            img:'/images/featured/featured_home_4.jpg',
+            lineOne:'Pearl Gray Musical Drumset',
+            lineTwo:'Buy the set get the Vic Firth sticks off half price',
+            linkTitle:'View discounts',
+            linkTo:'/shop'
+        },
+
+        {
+            img:'/images/featured/featured_home_5.jpg',
+            lineOne:'Microphone DJ Controller',
+            lineTwo:'Need a mic for tonight?',
+            linkTitle:'See the selection',
+            linkTo:'/shop'
+        },
+
+        
+
+
     ]
 
     return (
