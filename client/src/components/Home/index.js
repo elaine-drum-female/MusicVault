f09de0821d9216816967be185f2aeec803e6d539
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 02:30:43 -0500

    Imported HeroPromo component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import HeroSlider from './hero_slider';
+import HeroPromo from './hero_promo';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <HeroSlider/>
+                <HeroPromo/>
             </div>
         );
     }
