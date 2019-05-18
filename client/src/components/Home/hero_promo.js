53d97f9005b3345e3d7ff8d5224cfa6eba464733
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 02:25:26 -0500

    Created the entire HeroPromo component code

---
+++ client/src/components/Home/hero_promo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import LinkButton from '../utils/button';
+
+const HeroPromo = (props) => {
+
+    const promotion = {
+
+        img:'/images/featured/featured_home_3.jpg',
+        lineOne:'Up to 40% off',
+        lineTwo:'In second hand guitars',
+        linkTitle:'Shop now',
+        linkTo:'/shop'
+    }
+
+    const displayPromo = () => (
+        promotion ?
+        <div className="home_promotion_img">
+            style={{
+                background: `url(${promotion.img})`
+            }}
+
+            <div className="featured_action">
+                <div className="tag title">{promotion.lineOne}</div>
+                <div className="tag low_title">{promotion.lineTwo}</div>
+                <div>
+                    <LinkButton
+                        type="default"
+                        title={promotion.linkTitle}
+                        linkTo={promotion.linkTo}
+                        addStyles={{
+                            margin:'10px 0 0 0'
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+        : null 
+    )
+
+    return (
+        <div className="home_promotion">
+            { displayPromo() }
+        </div>
+    );
+};
+
+export default HeroPromo;
