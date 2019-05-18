effab62a30bf8ea275dacdf276b17546e2e98fbf
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 14:56:04 -0500

    Imported Shop component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+
 import Wrapper from './hoc/wrapper';
+import Authorize from './hoc/auth';
+
+import Home from './components/Home';
 import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/register';
+import Shop from './components/Shop';
+
 import UserDashboard from './components/PrivateDashboard';
-import Authorize from './hoc/auth';
+
 
 const App = () => {
   return (
     <Wrapper>
       <Switch>
         <Route exact path="/user/dashboard" component={Authorize(UserDashboard, true)} />
+
         <Route exact path="/register" component={Authorize(Register, false)} />
         <Route exact path="/register_login" component={Authorize(RegisterLogin, false)} />
+
         <Route exact path="/" component={Authorize(Home , null)} />
+        <Route exact path="/shop" component={Authorize(Shop , null)} />
       </Switch>
     </Wrapper>
   );
