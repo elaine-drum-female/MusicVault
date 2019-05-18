e83b1c867077b049afbb2a436185928549c5679b
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 10:25:40 -0500

    Added ;

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,5 +3,5 @@ export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'authorized_user';
 export const LOGOUT_USER = 'logout_user';
 
-export const PRODUCTS_BY_SELL = 'products_by_sell'
-export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival'
+export const PRODUCTS_BY_SELL = 'products_by_sell';
+export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival';
