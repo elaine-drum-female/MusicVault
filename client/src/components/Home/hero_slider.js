96dff1e437f97196be502ed6c51b7ec24853b7cc
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 01:14:47 -0500

    Added HeroSlider Component

---
+++ client/src/components/Home/hero_slider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const HeroSlider = (props) => {
+
+    const slides = [
+        {
+            img:'',
+            lineOne:'',
+            lineTwo:'',
+            linkTitle:'',
+            linkTo:''
+        },
+        
+        {
+            img:'',
+            lineOne:'',
+            lineTwo:'',
+            linkTitle:'',
+            linkTo:''
+        }
+    ]
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeroSlider;
