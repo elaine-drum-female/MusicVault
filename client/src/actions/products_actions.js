cd71200eeab1781ab1f9d84f065ba1d42c22fa4a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 10:29:52 -0500

    Imported PRODUCTS BY SELL AND BY ARRIVAL, also created functions

---
+++ client/src/actions/products_actions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { PRODUCTS_BY_SELL, PRODUCTS_BY_ARRIVAL } from './types';
+import { PRODUCT_SERVER } from '../components/utils/misc';
+
+
+export function ProductsBySell() {
+
+}
+
+export function ProductsByArrival() {
+
+}
