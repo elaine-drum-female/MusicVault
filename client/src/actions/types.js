8d50e57b44e84d4be2ceb9df284f9fd0eb9c9a4b
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 10:25:08 -0500

    Added PRODUCTS BY SELL, AND PRODUCTS BY ARRIVAL

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,6 @@ export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'authorized_user';
 export const LOGOUT_USER = 'logout_user';
+
+export const PRODUCTS_BY_SELL = 'products_by_sell'
+export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival'
