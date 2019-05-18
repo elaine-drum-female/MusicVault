33587247b1157ac792c850ed662e2875dd555253
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 15:18:27 -0500

    Created the Shop component

---
+++ client/src/components/Shop/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PageTop from '../utils/page_top';
+
+import { connect } from 'react-redux';
+
+
+class Shop extends Component {
+
+
+    render() {
+        const products = this.props.products;
+
+        return (
+            <div>
+                <PageTop 
+                    title="Browse Products"
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect()(Shop);
