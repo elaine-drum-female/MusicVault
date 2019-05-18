7642e603f8d60a6dfefb7a349a97e015e88f22a6
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 01:48:25 -0500

    Imported HeroSlider Component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import HeroSlider from './hero_slider';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                home
+                <HeroSlider/>
             </div>
         );
     }
