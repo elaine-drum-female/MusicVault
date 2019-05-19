67b6eacfdafbcd9198792a86d2d2d9146b884873
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 22:58:37 -0500

    Imported a lot of FontAwesome files and all of list from material-ui... plus created a state and componentDidMount()

---
+++ client/src/components/utils/radiobuttonCollapse.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faAngleDown from '@fortawesome/fontawesome-free-solid/faAngleDown';
+import faAngleUp from '@fortawesome/fontawesome-free-solid/faAngleUp';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+class RadiobuttonCollapse extends Component {
+
+    state = {
+        open: false,
+        value:'0'
+    }
+
+    
+    componentDidMount(){
+        if(this.props.initState){
+            this.setState({
+                open: this.props.initState
+            })
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default RadiobuttonCollapse;
