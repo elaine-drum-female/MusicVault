ce363ff8533ba9b0833375069e6d7bdab1dc8c3e
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 23:25:28 -0500

    Added handlePrice function

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -29,6 +29,18 @@ class Shop extends Component {
         this.props.dispatch(fetchWoods());
     }
 
+    handlePrice = (value) => {
+        const data = price;
+        let array = [];
+
+        for(let key in data){
+            if(data[key]._id === parseInt(value,10)){
+                array = data[key].array
+            }
+        }
+        return array;
+    }
+
     handleFilters = (filters,category) => {
       const newFilters = {...this.state.filters}
       newFilters[category] = filters;
