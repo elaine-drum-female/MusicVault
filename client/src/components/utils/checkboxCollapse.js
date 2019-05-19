8a78171ec4483fb3946124d12d8ae4ff2100df43
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 21:20:55 -0500

    Created the List component from line 32

--- client/src/components/utils/checkboxCollapse.js
+++ client/src/components/utils/checkboxCollapse.js
@@ -25,10 +25,25 @@ class CheckboxCollapse extends Component {
         }
     }
 
+    
+
     render() {
         return (
-            <div>
-                
+            <div className="collapse_items_wrapper">
+                 <List style={{borderBottom: '1px solid #dbdbdb'}}>
+                    <ListItem onClick={this.handleClick} style={{padding:'10px 23px 10px 0'}}>
+                        <ListItemText
+                            primary={this.props.title}
+                            className="collapse_title"
+                        />
+                        {this.handleAngle()}
+                    </ListItem>
+                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            {this.renderList()}
+                        </List>
+                    </Collapse>
+                </List>
             </div>
         );
     }
