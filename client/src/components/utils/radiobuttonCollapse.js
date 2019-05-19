a41acebe2a06e6a438a68b4205283bcee10fe616
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-18 23:17:40 -0500

    Rendered the list, created handleChange,  added same List tags plus a RadioGroup component

--- client/src/components/utils/radiobuttonCollapse.js
+++ client/src/components/utils/radiobuttonCollapse.js
@@ -44,11 +44,49 @@ class RadiobuttonCollapse extends Component {
             />
     )
 
+    renderList = () => (
+        this.props.list ?
+            this.props.list.map( value =>(
+                <FormControlLabel
+                    key={value._id}
+                    value={`${value._id}`}
+                    control={<Radio/>}
+                    label={value.name}
+                />
+            ))
+        :null
+    )
+
+    handleChange = event => {
+        this.props.handleFilters(event.target.value)
+        this.setState({value: event.target.value})
+    }
+
 
     render() {
         return (
             <div>
-                
+                <List style={{borderBottom: '1px solid #dbdbdb'}}>
+                    <ListItem onClick={this.handleClick} style={{padding:'10px 23px 10px 0'}}>
+                        <ListItemText
+                            primary={this.props.title}
+                            className="collapse_title"
+                        />
+                        {this.handleAngle()}
+                    </ListItem>
+                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                              <RadioGroup
+                                aria-label="prices"
+                                name="prices"
+                                value={this.state.value}
+                                onChange={this.handleChange}
+                            >
+                                { this.renderList() }
+                            </RadioGroup>
+                        </List>
+                    </Collapse>
+                </List>
             </div>
         );
     }
