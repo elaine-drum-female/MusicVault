0c825e3088ce54ed3c239adee0870c4b8a7819f0
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 17:22:36 -0500

    Added the state object as well as imported FormField, update, generateData,isFormValid...connect,fetchBrands, fetchWoods

--- client/src/components/PrivateDashboard/Admin/add_product.js
+++ client/src/components/PrivateDashboard/Admin/add_product.js
@@ -1,13 +1,211 @@
 import React, { Component } from 'react';
+import UserLayout from '../../../hoc/userlayout';
+
+import FormField from '../../utils/Form/formfield';
+import { update, generateData, isFormValid } from '../../utils/Form/formActions';
+
+import { connect } from 'react-redux';
+import { fetchBrands, fetchWoods } from '../../../actions/products_actions';
 
 class AddProduct extends Component {
+
+    state = {
+        formError: false,
+        formSuccess: false,
+        formData: {
+            name: {
+                element: 'input',
+                value: '',
+                config:{
+                    label: 'Product name',
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Please enter your name'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showLabel: true
+            },
+
+            description: {
+                element: 'textarea',
+                value: '',
+                config:{
+                    label: 'Product Description',
+                    name: 'description_input',
+                    type: 'text',
+                    placeholder: 'Please enter your description'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showLabel:true
+            },
+
+            price: {
+                element: 'input',
+                value: '',
+                config:{
+                    label: 'Product Price',
+                    name: 'price_input',
+                    type: 'number',
+                    placeholder: 'Please enter your price'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showLabel:true
+            }
+            ,
+            brand: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Product Brand',
+                    name: 'brands_input',
+                    options:[]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showLabel:true
+            },
+
+            shipping: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Shipping',
+                    name: 'shipping_input',
+                    options:[
+                        {key:true,value:'Yes'},
+                        {key:false,value:'No'},
+                    ]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            available: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Available, in stock',
+                    name: 'available_input',
+                    options:[
+                        {key:true,value:'Yes'},
+                        {key:false,value:'No'},
+                    ]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            wood: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Wood material',
+                    name: 'wood_input',
+                    options:[]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            frets: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Frets',
+                    name: 'frets_input',
+                    options:[
+                        {key:20,value:20},
+                        {key:21,value:21},
+                        {key:22,value:22},
+                        {key:24,value:24}
+                    ]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            publish: {
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Publish',
+                    name: 'publish_input',
+                    options:[
+                        {key:true,value:'Public'},
+                        {key:false,value:'Hidden'},
+                    ]
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            images:{
+                value:[],
+                validation:{
+                    required: false
+                },
+                valid: true,
+                touched: false,
+                validationMessage:'',
+                showlabel: false
+            }
+        }
+    }
+
     render() {
         return (
-            <div>
-                add product
-            </div>
+            <UserLayout>
+                <div>
+                    add product
+                </div>
+            </UserLayout>
         );
     }
 }
 
-export default AddProduct;
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(AddProduct);
