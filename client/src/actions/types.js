49aa7c879e6fee5f78a183371cdc3ebcafa40b63
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 18:38:50 -0500

    New export ADD_PRODUCT

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,4 @@ export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival';
 export const FETCH_BRANDS = 'fetch_brands';
 export const FETCH_WOODS = 'fetch_woods';
 export const FETCH_PRODUCTS_TO_SHOP = "fetch_products_to_shop";
+export const ADD_PRODUCT = "add_product";
