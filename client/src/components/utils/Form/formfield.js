c5c4c315a4ae1865cd974bbe15ad092060ef9dad
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 17:45:13 -0500

    Added showlabel, case select and case textarea

--- client/src/components/utils/Form/formfield.js
+++ client/src/components/utils/Form/formfield.js
@@ -25,6 +25,10 @@ const Formfield = ({id, formdata, change}) => {
             case('input'):
                 formTemplate = (
                     <div className="formBlock">
+                         { formdata.showlabel ? 
+                            <div className="label_inputs">{formdata.config.label}</div>
+                        :null}
+
                         <input
                             {...formdata.config}
                             value={formdata.value}
@@ -35,6 +39,49 @@ const Formfield = ({id, formdata, change}) => {
                         {showError()}
                     </div>
                 )
+            break;
+            case('select'):
+                formTemplate = (
+                    <div className="formBlock">
+                        { formdata.showlabel ? 
+                            <div className="label_inputs">{formdata.config.label}</div>
+                        :null}
+                        <select
+                            value={formdata.value}
+                            onBlur={(event)=> change({event,id,blur:true})}
+                            onChange={(event)=> change({event,id}) }
+                        >
+                            <option value="">Select one</option>
+                            {
+                                formdata.config.options.map(item=>(
+                                    <option 
+                                        key={item.key} 
+                                        value={item.key}
+                                    >
+                                        {item.value}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                        {showError()}
+                    </div>
+                )
+            break;
+            case('textarea'):
+                formTemplate = (
+                    <div className="formBlock">
+                    { formdata.showlabel ? 
+                        <div className="label_inputs">{formdata.config.label}</div>
+                    :null}
+                    <textarea
+                        {...formdata.config}
+                        value={formdata.value}
+                        onBlur={(event)=> change({event,id,blur:true})}
+                        onChange={(event)=> change({event,id}) }
+                    />
+                        {showError()}
+                    </div>
+            )
             break;
             default:
                 formTemplate = null;
