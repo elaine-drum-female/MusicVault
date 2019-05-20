efa9fbc52412af799a8001c7a7d4cd5c061788d3
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 16:27:02 -0500

    Added the findArgs['publish'] array to filter out ones that aren's avail

--- server/server.js
+++ server/server.js
@@ -56,6 +56,9 @@ app.post('/api/product/shop', (req, res) => {
 
         }
     }
+
+        findArgs['publish'] = true;
+            
         Product.
         find(findArgs).
         populate('brand').
