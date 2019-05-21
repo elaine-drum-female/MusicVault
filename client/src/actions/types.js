32129b89b3217cd2325c610cf09778676040eae2
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 23:24:43 -0500

    Export ADD_BRAND

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,6 +6,8 @@ export const LOGOUT_USER = 'logout_user';
 export const PRODUCTS_BY_SELL = 'products_by_sell';
 export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival';
 export const FETCH_BRANDS = 'fetch_brands';
+export const ADD_BRAND =  "add_brand";
+
 export const FETCH_WOODS = 'fetch_woods';
 export const FETCH_PRODUCTS_TO_SHOP = "fetch_products_to_shop";
 export const ADD_PRODUCT = "add_product";
