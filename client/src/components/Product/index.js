4e4a77b7234c6af7b9e4b220648aa3421b1806c3
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-21 18:31:43 -0500

    Added back in getProductDetail, clearProductDetail, ProdInfo, and ProdImage

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
-
+import { getProductDetail , clearProductDetail} from '../../actions/products_actions';
 import { connect } from 'react-redux';
+import ProdInfo from './prodInfo';
+import ProdImage from './prodImage';
 
 
 class ProductPage extends Component {
 
     componentDidMount(){
         const id = this.props.match.params.id;
-        console.log(id);
+        this.props.dispatch(getProductDetail(id)).then(()=>{
+            if(!this.props.products.prodDetail){
+                this.props.history.push('/');
+            }
+        })
     }
 
-    
+    componentWillUnmount(){
+        this.props.dispatch(clearProductDetail())
+    }
     
     render() {
         return (
             <div>
                 <PageTop
                     title="Product detail"
-                />               
+                />  
+                <div className="container">
+                {
+                    this.props.products.prodDetail ?
+                    <div className="product_detail_wrapper">
+                        <div className="left">
+                            <div style={{width:'500px'}}>
+                                <ProdImage
+                                    detail={this.props.products.prodDetail}
+                                />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <ProdInfo
+                                addToCart={(id)=> this.addToCartHandler(id)}
+                                detail={this.props.products.prodDetail}
+                            />
+                        </div>
+                    </div>
+                    : 'Loading'
+                }
+
+                </div>                   
             </div>
         );
     }
