0e5bd36529cc35e76a8e5bdb2abe8571b998e3b8
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-20 23:07:34 -0500

    Created ManageBrands component

---
+++ client/src/components/PrivateDashboard/Admin/manage_brands.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ManageBrands extends Component {
+    render() {
+        return (
+            <div>
+                brands
+            </div>
+        );
+    }
+}
+
+export default ManageBrands;
