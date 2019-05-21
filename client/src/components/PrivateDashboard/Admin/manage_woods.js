4959ef3df59859c6fc02a4123456d76859ffb764
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-21 00:36:46 -0500

    Added the state, and imported FormField, update, generateData, isFormValid, resetFields, connect and fetchWoods

--- client/src/components/PrivateDashboard/Admin/manage_woods.js
+++ client/src/components/PrivateDashboard/Admin/manage_woods.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import FormField from '../../utils/Form/formfield';
+import { update, generateData, isFormValid,resetFields} from '../../utils/Form/formActions';
+
+import { connect } from 'react-redux';
+import { fetchWoods } from '../../../actions/products_actions';
 
 class ManageWoods extends Component {
+
+    state = {
+        formError:false,
+        formSuccess:false,
+        formdata:{
+            name: {
+                element: 'input',
+                value: '',
+                config:{
+                    label: 'Brand name',
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Enter the wood'
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
+            },
+        }
+    }
+
     render() {
         return (
             <div>
