a422d4f84c5a703c587322c7a32b2d3fa5e1fb26
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 13:43:10 -0500

    Fixed extra space for ON SUCCESS BUY USER

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,7 +4,7 @@ export const AUTH_USER = 'authorized_user';
 export const LOGOUT_USER = 'logout_user';
 export const ADD_TO_CART_USER  = "add_to_cart_user";
 export const GET_CART_ITEMS_USER = "get_card_items_user";
-export const ON_SUCCESS_BUY_USER  =" on_success_buy_user";
+export const ON_SUCCESS_BUY_USER  = "on_success_buy_user";
 
 export const PRODUCTS_BY_SELL = 'products_by_sell';
 export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival';
