087cd77cddbf406cd09e762db3ec8a4f4ed96655
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 18:28:09 -0500

    Exported default function

---
+++ client/src/reducers/site_reducer.js
@@ -0,0 +1,12 @@
+// import { 
+    
+
+//  } from './types';
+
+//Return the state
+export default function( state= {} , action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
