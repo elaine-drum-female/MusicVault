b744b5212e4ee8ec866c70ab5919eb87a45b32cb
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 01:23:10 -0500

    Created UserCart component

---
+++ client/src/components/PrivateDashboard/cart.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import UserLayout from '../../hoc/userlayout';
+
+import { connect } from 'react-redux';
+import FontAWesomeIcon from '@fortawesome/react-fontawesome';
+import faFrown from '@fortawesome/fontawesome-free-solid/faFrown'
+import faSmile from '@fortawesome/fontawesome-free-solid/faSmile'
+
+class UserCart extends Component {
+    render() {
+        return (
+            <UserLayout>
+                <div>
+                    cart
+                </div>
+            </UserLayout>
+        );
+    }
+}
+
+const mapToStateProps = ( state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapToStateProps )(UserCart);
