ce87b19103069fcd894a3bf5a1e2be9783788b0f
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 18:05:36 -0500

    Created UpdateSiteInfo component

---
+++ client/src/components/PrivateDashboard/Admin/update_site_info.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class UpdateSiteInfo extends Component {
+    render() {
+        return (
+            <div>
+                site info
+            </div>
+        );
+    }
+}
+
+export default UpdateSiteInfo;
