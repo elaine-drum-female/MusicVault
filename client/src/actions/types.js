bac5097c65234656fefa7b1d576fbe1f51bfeefe
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-21 19:53:00 -0500

    Exported ADD_TO_CART_USER

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,6 +2,7 @@ export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'authorized_user';
 export const LOGOUT_USER = 'logout_user';
+export const ADD_TO_CART_USER  = "add_to_cart_user";
 
 export const PRODUCTS_BY_SELL = 'products_by_sell';
 export const PRODUCTS_BY_ARRIVAL = 'products_by_arrival';
