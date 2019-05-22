2b4fd851555ebe047d411a71727eabc73680e401
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 02:05:23 -0500

    Added componentDidMount for when there are items in the cart to display them all on the page

--- client/src/components/PrivateDashboard/cart.js
+++ client/src/components/PrivateDashboard/cart.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import UserLayout from '../../hoc/userlayout';
 
 import { connect } from 'react-redux';
-import FontAWesomeIcon from '@fortawesome/react-fontawesome';
-import faFrown from '@fortawesome/fontawesome-free-solid/faFrown'
-import faSmile from '@fortawesome/fontawesome-free-solid/faSmile'
+import { getCartItems} from '../../actions/user_actions';
+
+// import FontAWesomeIcon from '@fortawesome/react-fontawesome';
+// import faFrown from '@fortawesome/fontawesome-free-solid/faFrown'
+// import faSmile from '@fortawesome/fontawesome-free-solid/faSmile'
 
 class UserCart extends Component {
 
@@ -15,7 +17,24 @@ class UserCart extends Component {
         showSuccess: false,
     }
 
-    
+    componentDidMount(){
+        let cartItems = [];
+        let user = this.props.user;
+
+        if(user.userData.cart){
+            if(user.userData.cart.length > 0){
+                user.userData.cart.forEach(item=>{
+                    cartItems.push(item.id)
+                });
+                this.props.dispatch(getCartItems(cartItems,user.userData.cart))
+                .then(()=>{
+                    // if(this.props.user.cartDetail.length > 0){
+                    //     this.calculateTotal(this.props.user.cartDetail);
+                    // }
+                })
+            }
+        }
+    }
 
     render() {
         return (
