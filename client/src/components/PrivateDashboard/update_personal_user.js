7daa24bfdc9278eed428a69d53f4b370d19b0bc4
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 16:59:02 -0500

    Created UpdatePersonalInfo component

---
+++ client/src/components/PrivateDashboard/update_personal_user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UpdatePersonalInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UpdatePersonalInfo;
