819d8dd2c6582a86b4ebfbe244c03021eb2235bb
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 18:54:45 -0500

    Imported GET SITE DATA

--- client/src/reducers/site_reducer.js
+++ client/src/reducers/site_reducer.js
@@ -1,11 +1,12 @@
-// import { 
-    
-
-//  } from './types';
+import { 
+    GET_SITE_DATA
+} from './types';
 
 //Return the state
 export default function( state= {} , action) {
     switch(action.type) {
+        case GET_SITE_DATA:
+            return {...state, siteData: action.payload}
         default:
             return state;
     }
