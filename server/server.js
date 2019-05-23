3b7041659f503d3c59495a7135196dbe27a95807
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 20:46:45 -0500

    Updated mongoose connect with process env MONGODB_URI only

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/waves");
+mongoose.connect(process.env.MONGODB_URI);
 
 // Models
 const { User } = require('./models/user');
