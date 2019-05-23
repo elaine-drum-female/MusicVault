ddb2daa0ffdb2982c10c585b8b8387b2b60ac705
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-22 19:37:09 -0500

    Added connect getSiteData and componentDidMount

--- client/src/hoc/wrapper.js
+++ client/src/hoc/wrapper.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import Header from '../components/Header_Footer/Header';
 import Footer from '../components/Header_Footer/Footer';
 
+import { connect } from 'react-redux';
+import { getSiteData } from '../actions/site_actions';
+
 class Wrapper extends Component {
+
+    componentDidMount(){
+        if(Object.keys(this.props.site).length === 0){
+            this.props.dispatch(getSiteData());
+        }
+    }
+
     render() {
         return (
             <div>
@@ -10,10 +20,16 @@ class Wrapper extends Component {
                 <div className="page_container">
                     {this.props.children}
                 </div>
-                <Footer/>
+                <Footer data={this.props.site}/>
             </div>
         );
     }
 }
 
-export default Wrapper;
+const mapStateToProps = (state => {
+    return {
+        site: state.site
+    }
+})
+
+export default connect(mapStateToProps)(Wrapper);
